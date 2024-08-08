36a532bb91e4b69379c6e77521d8ac7b45f36455
Kimwonjoon <kimpass189@gmail.com>
2024-08-08 16:46:13 +0900

    spark_dag_add

---
+++ dags/pyspark_movie.py
@@ -0,0 +1,79 @@
+from datetime import datetime, timedelta
+from textwrap import dedent
+
+from airflow import DAG
+
+from airflow.operators.bash import BashOperator
+from airflow.operators.empty import EmptyOperator
+from airflow.models import Variable
+from airflow.operators.python import (
+        PythonOperator, 
+        PythonVirtualenvOperator, 
+        BranchPythonOperator
+        )
+
+with DAG(
+    'pyspark_movie',
+    default_args={
+        'depends_on_past': False,
+        'retries': 1,
+        'retry_delay': timedelta(seconds=3),
+    },
+    description='hello world DAG',
+    schedule="10 2 * * *",
+    start_date=datetime(2015, 1, 1),
+    end_date=datetime(2016,1,1),
+    catchup=True,
+    tags=["pyspark"],
+    max_active_runs=1,  # 동시에 실행될 수 있는 최대 DAG 인스턴스 수
+#    max_active_tasks=3,  # 동시에 실행될 수 있는 최대 태스크 수
+) as dag:
+    def re_parti(ds_nodash):
+        from spark_repart.re_part import re_part
+        re_part(ds_nodash)
+
+    def branch_func(ds_nodash):
+        import os
+        home_dir = os.path.expanduser("~")
+        path = f'{home_dir}/data/movie/repartition/load_dt={ds_nodash}'
+        # 이미 파일이 있다면 join_df로
+        if os.path.exists(path):
+            return join_df.task_id
+        # 파일이 없다면 re_partition로
+        else:
+            return re_partition.task_id
+
+    start = EmptyOperator(task_id = 'start')
+    end = EmptyOperator(task_id = 'end')
+
+    branch_op = BranchPythonOperator(
+            task_id="branch_op",
+            python_callable=branch_func
+            )
+    
+#    rm_dir = BashOperator(
+#            task_id='rm.dir',
+#            bash_command='rm -rf /home/kimpass189/data/movie/repartition/load_dt={{ ds_nodash }}',
+#            trigger_rule = "all_success"
+#    )
+
+    re_partition = PythonOperator(
+            task_id = "re.partition",
+            python_callable=re_parti)
+    join_df = BashOperator(
+        task_id='join.df',
+        bash_command="""
+            spark-submit $AIRFLOW_HOME/pyspark_df/movie_join_df.py {{ds_nodash}}
+            """,
+        trigger_rule='all_done'
+        )
+    agg = BashOperator(
+        task_id='agg',
+        bash_command="""
+            spark-submit $AIRFLOW_HOME/pyspark_df/sum_multi.py {{ds_nodash}}
+            """,
+        trigger_rule='all_done'
+        )
+            
+    start  >> branch_op >> [join_df,re_partition]
+    re_partition >> join_df >> agg >> end
---
+++ pyspark_df/sum_multi.py
@@ -0,0 +1,35 @@
+from pyspark.sql import SparkSession
+import sys
+LOAD_DT = sys.argv[1]
+
+spark = SparkSession.builder.appName("Sum_multi").getOrCreate()
+
+df1 = spark.read.parquet(f"/home/kimpass189/data/movie/hive/load_dt={LOAD_DT}")
+df1.createOrReplaceTempView("first_day")
+
+df_n = spark.sql(f"""
+SELECT
+    sum(salesAmt) as sum_salesAmt,
+    sum(audiCnt) as sum_audiCnt,
+    sum(showCnt) as sum_showCnt,
+    multiMovieYn,
+    '{LOAD_DT}' AS load_dt
+FROM first_day
+GROUP BY multiMovieYn
+""")
+df_n.write.mode('overwrite').partitionBy("load_dt").parquet("/home/kimpass189/data/movie/sum-multi")
+
+df_r = spark.sql(f"""
+SELECT
+    sum(salesAmt) as sum_salesAmt,
+    sum(audiCnt) as sum_audiCnt,
+    sum(showCnt) as sum_showCnt,
+    repNationCd,
+    '{LOAD_DT}' AS load_dt
+FROM first_day
+GROUP BY repNationCd
+""")
+
+df_r.write.mode('overwrite').partitionBy("load_dt").parquet("/home/kimpass189/data/movie/sum-nation")
+
+spark.stop()
